9c89fe3428265d0e55a78844fc342e862cae7f5e
藤井泰輔 <izumi@izumiBook-Pro.local>
2026-06-11 10:08:27 +0900

    notes

--- routes/notes.js
+++ routes/notes.js
@@ -2,19 +2,20 @@ var express = require('express');
 var router = express.Router();
 // レスポンスのデータ（ノート全件）
 const responseObjectDataAll = {
-textObject1 : {
-id: 1,
-title: 'ノート１のタイトルです',
-subTitle: 'ノート１のサブタイトルです',
-bodyText: 'ノート１の本文です'
-},
-textObject2 : {
-id: 2,
-title: 'ノート２のタイトルです',
-subTitle: 'ノート２のサブタイトルです',
-bodyText: 'ノート２の本文です'
-},
+ textObject1 : {
+  id: 1,
+  title: 'ノート１のタイトルです',
+  subTitle: 'ノート１のサブタイトルです',
+  bodyText: 'ノート１の本文です'
+ },
+ textObject2 : {
+  id: 2,
+  title: 'ノート２のタイトルです',
+  subTitle: 'ノート２のサブタイトルです',
+  bodyText: 'ノート２の本文です'
+ },
 };
+
 /**
 * メモを全件取得するAPI
 * @returns {Object[]} data
@@ -23,7 +24,8 @@ bodyText: 'ノート２の本文です'
 * @returns {string} data.text - 内容
 */
 router.get('/', function (req, res, next) {
-// 全件取得して返す
-res.json(responseObjectDataAll);
+ // 全件取得して返す
+ res.json(responseObjectDataAll);
 })
+
 module.exports = router;
